521e7e956f61c5887c9cca519e45ee2824573406
natke <natke@users.noreply.github.com>
2023-02-17 01:34:06 +0000

    Update Rust API docs to commit b64137d

--- docs/api/rust/crates.js
+++
@@ -1,1 +0,0 @@
-window.ALL_CRATES = ["onnxruntime","onnxruntime_sys"];
--- docs/api/rust/implementors/onnxruntime/trait.Utf8Data.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"onnxruntime":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
